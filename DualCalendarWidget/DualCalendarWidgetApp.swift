7c8b5c05a1f31f41566a16f8e56b771109c2441f
AISAKI <2228387060@qq.com>
2026-06-26 16:03:32 +0800

    Initial Commit

---
+++ DualCalendarWidget/DualCalendarWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  DualCalendarWidgetApp.swift
+//  DualCalendarWidget
+//
+//  Created by AISAKI on 2026/6/26.
+//
+
+import SwiftUI
+
+@main
+struct DualCalendarWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
